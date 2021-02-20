9a23ee0d0e4975b25fbabba9cb7533a5559c9e58
David Dales <david.c.dales@gmail.com>
2021-02-20 15:48:18 -0500

    more responsibilities for Note component

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,19 +1,49 @@
+import { useState } from "react";
+
 export default function Note(props) {
-  function handleClick(e) {
-    console.log(`play this note: ${props.note}`);
+  // const [mouseIsDown, setMouseDownState] = useState(false);
+  const [classNames, setClassNames] = useState(
+    props.note.includes("#") ? "note sharp" : "note"
+  );
+  // props.mouseIsDown ?
+  // let classNames = props.note.includes("#") ? "note sharp" : "note";
+
+  function handleMouseDown(e) {
     props.synth.triggerAttackRelease(`${[props.note]}4`, "8n");
+    setClassNames(
+      props.note.includes("#") ? "note sharp active" : "note active"
+    );
+  }
+
+  function handleMouseUp() {
+    setClassNames(props.note.includes("#") ? "note sharp" : "note");
   }
 
   function handleMouseOver(e) {
     if (props.mouseIsDown) {
-      console.log(`play this note: ${props.note}`);
       props.synth.triggerAttackRelease(`${[props.note]}4`, "8n");
+      setClassNames(
+        props.note.includes("#") ? "note sharp active" : "note active"
+      );
+    }
+  }
+
+  function handleMouseLeave(e) {
+    if (props.mouseIsDown) {
+      props.synth.triggerAttackRelease(`${[props.note]}4`, "8n");
+      setClassNames(props.note.includes("#") ? "note sharp" : "note");
     }
   }
 
   return (
-    <div onClick={handleClick} onMouseOver={handleMouseOver}>
-      {props.note}
+    <div
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseLeave}
+      className={classNames}
+    >
+      {/* {props.note} */}
     </div>
   );
 }
