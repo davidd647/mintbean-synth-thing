bf525d21921387b8da4e02180e7297ec01331fa1
David Dales <david.c.dales@gmail.com>
2021-02-21 15:07:51 -0500

    sharp keys and.. non-sharp keys handled separately

--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "../keyboardsass.scss";
 
 import Note from "./Note";
+import Sharp from "./Sharp";
 
 // index.js
 
@@ -16,32 +17,19 @@ const synth = new Tone.Synth().toDestination();
 // synth.triggerAttackRelease("C4", "8n");
 
 export default function Keyboard() {
-  const NOTES = [
-    "C",
-    "C#",
-    "D",
-    "D#",
-    "E",
-    "F",
-    "F#",
-    "G",
-    "G#",
-    "A",
-    "A#",
-    "B",
-  ];
+  const NOTES = ["C", "D", "E", "F", "G", "A", "B"];
+
+  const SHARPS = ["C#", "D#", "F#", "G#", "A#"];
 
   // mouseIsDown might need to be inside state... so use hooks?
   // https://www.youtube.com/watch?v=O6P86uwfdR0&t=0s&ab_channel=WebDevSimplified
   const [mouseIsDown, setMouseDownState] = useState(false);
 
   function handleMouseDown() {
-    console.log("mouse is down");
     setMouseDownState(true);
   }
 
   function handleMouseUp() {
-    console.log("mouse is up");
     setMouseDownState(false);
   }
 
@@ -51,8 +39,27 @@ export default function Keyboard() {
       onMouseUp={handleMouseUp}
       className="keyboard"
     >
-      {NOTES.map((note) => {
-        return <Note note={note} mouseIsDown={mouseIsDown} synth={synth} />;
+      {NOTES.map((note, i) => {
+        return (
+          <Note
+            key={`note${i}`}
+            note={note}
+            mouseIsDown={mouseIsDown}
+            synth={synth}
+            index={i}
+          />
+        );
+      })}
+      {SHARPS.map((sharp, i) => {
+        return (
+          <Sharp
+            key={`sharp${i}`}
+            note={sharp}
+            mouseIsDown={mouseIsDown}
+            synth={synth}
+            index={i}
+          />
+        );
       })}
     </div>
   );
